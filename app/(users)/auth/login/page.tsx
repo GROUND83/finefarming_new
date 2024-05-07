cde4de86991bb77b845fa1b88ac2dc96a8eaef37
wonchang <ground83@wonchangui-MacBookPro.local>
2024-05-08 00:21:47 +0900

    accout2

--- app/(users)/auth/login/page.tsx
+++ app/(users)/auth/login/page.tsx
@@ -5,13 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
-import {
-  PASSWORD_ERROR_MESSAGE,
-  PASSWORD_MIN_LENGHT,
-  PASSWORD_REGEX,
-  PASSWORD_REGEX_ERROR,
-} from "@/lib/constants";
+
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -23,14 +17,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { signIn } from "next-auth/react";
+import { formSchema } from "./loginSchema";
 
-export const formSchema = z.object({
-  email: z.string().email({ message: "이메일을 입력하세요." }),
-  password: z
-    .string()
-    .min(PASSWORD_MIN_LENGHT, PASSWORD_ERROR_MESSAGE)
-    .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-});
 export default function Login() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
